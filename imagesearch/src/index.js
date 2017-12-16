4215217f393401d5ac982a094733f2eb0cb4629f
Shaniqua <zahirbasrai@yahoo.com>
2017-12-15 20:00:36 -0800

    imagesearch complete

--- imagesearch/src/index.js
+++ imagesearch/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function Imagegallery(props) {
+  return(
+    <div className ="imagesearch">
+      <h1>Image Gallery</h1>
+      <ul>
+        {props.images.map((images) => <li key={images.id}><img src={images.url} /> {images.title}
+         </li>
+       )}
+      </ul>
+    </div>        
+  )
+}
+
+const images = [
+  {id: 1, title: 'Aortic Dissection', content: 'Welcome to learning React!', url: 'https://wikem.org/w/images/AoDissect_DeBakey1.png'},
+  {id: 2, title: 'Another Aortic Dissection', content: 'You can install React from npm.', url:'https://wikem.org/w/images/AoDissect_DeBakey2.png'}
+];
+
+ReactDOM.render(
+<Imagegallery images={images} />, document.getElementById('root')
+);
 registerServiceWorker();
